4aea1d47bf8465f9276e33f376a28bdc285ccb60
totono <thisistotono@gmail.com>
2023-10-13 00:56:35 +0900

    test api data fetching

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,12 +3,31 @@
 import { Button } from "ui";
 
 import styles from "../styles/index.module.css";
+import { useState } from "react";
+import { getMessageFromAPI } from "./MessageAPI";
 
 export default function Web() {
+
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleClick = async () => {
+    const result = await getMessageFromAPI();
+    if(result){
+      setMessage(result);
+
+    }else{
+      setError("something wrong");
+    }
+
+  }
+
   return (
     <div className={styles.container}>
       <h1>this is release test</h1>
-      <Button onClick={() => console.log("Pressed!")} text="Boop" />
+      <Button onClick={handleClick} text="Say Hi to Laravel." />
+      {message && <p>{message}</p>}
+      {error && <p>{error}</p>}
     </div>
   );
 }
